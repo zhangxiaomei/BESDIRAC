9ef2f53ac2c23d14034e2bb03c13d6c309af58bc
mirguest <lintao@ihep.ac.cn>
2013-03-11 19:05:57 +0800

    A typo error.

--- TransferSystem/Service/TransferRequestHandler.py
+++ TransferSystem/Service/TransferRequestHandler.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.DISET.RequestHandler import RequestHandler
+
+def initializeTransferRequestHandler(serviceInfo):
+  """ initialize handler """
+
+  gLogger.info("Initialize TransferRequestHandler.")
+
+  return S_OK()
+
+class TransferRequestHandler(RequestHandler):
+  """
+  This is for:
+    * create a request to transfer data
+  """
+
+  types_serverIsOK = []
+  def export_serverIsOK(self):
+    return S_OK()
