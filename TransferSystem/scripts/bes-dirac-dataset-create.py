c5d8ecf926598c2b653ed40179eae97fdf6c69ee
mirguest <lintao@ihep.ac.cn>
2013-03-13 19:59:36 +0800

    a new script to create new dataset.

--- TransferSystem/scripts/bes-dirac-dataset-create.py
+++ TransferSystem/scripts/bes-dirac-dataset-create.py
@@ -1,15 +1,41 @@
 # -*- coding: utf-8 -*-
 
 import DIRAC
+from DIRAC import gLogger
 from DIRAC.Core.Base import Script
 
+Script.setUsageMessage("""
+Create a dataset with a list of LFNs
+
+Usage:
+  %s <new dataset name> <filelist>
+""" % Script.scriptName)
+
+Script.registerSwitch("f:", "file=", "file contains LFN")
+
 Script.parseCommandLine( ignoreErrors = True )
+
+filelist = []
+
+for k,v in Script.getUnprocessedSwitches():
+  if k.lower() in ["f", "file"]:
+    with open(v) as f:
+      for line in f:
+        filelist.append(line)
+
+args = Script.getPositionalArgs()
+if ( len(args) == 0 or (len(filelist)+len(args) < 2) ):
+  gLogger.error("Please support dataset name and LFNs")
+  DIRAC.exit(-1)
+dataset = args[0]
+filelist.extend(args[1:])
+
+gLogger.info("Dataset is ", dataset)
+gLogger.info("FileList is ", filelist)
 
 from DIRAC.Core.DISET.RPCClient import RPCClient
 
 transferRequest = RPCClient("Transfer/Dataset")
 
-dataset = "my-dataset"
-filelist = map(str, range(10))
 print transferRequest.create(dataset, filelist)
 
