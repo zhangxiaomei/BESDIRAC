823f9d9c9617718c05e89d9d4a8e0bb2463c8f73
mirguest <lintao@ihep.ac.cn>
2013-03-13 09:45:47 +0800

    Insert into the Transfer Request Table.

--- TransferSystem/DB/TransferDB.py
+++ TransferSystem/DB/TransferDB.py
@@ -3,7 +3,39 @@
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR, Time
 from DIRAC.Core.Base.DB import DB
 
-class TransferDB(DB):
+# Some basic arguments will use namedtuple 
+from collections import namedtuple
+import datetime
 
-  def __init__(self, dbname="TransferDB", fullname="Transfer/TransferDB"):
-    DB.__init__(self, dbname, fullname)
+TransRequestEntry = namedtuple('TransRequestEntry',
+                              [#'id',
+                               'username',
+                               'submit_time',
+                               ])
+
+class TransferDB(DB):
+  tables = dict(TransferRequest = "TransferRequest",
+                TransferFileList = "TransferFileList")
+
+  def __init__(self, dbname="TransferDB", 
+                     fullname="Transfer/TransferDB",
+                     maxQueueSize = 10):
+    DB.__init__(self, dbname, fullname, maxQueueSize)
+
+  def insert_TransferRequest(self, entry):
+    if not isinstance(entry, TransRequestEntry):
+      raise TypeError("entry should be TransRequestEntry")
+    infoDict = entry._asdict()
+    res = self.insertFields( self.tables["TransferRequest"],
+                             inDict = infoDict)
+    return res
+
+if __name__ == "__main__":
+  from DIRAC.Core.Base import Script
+  Script.parseCommandLine( ignoreErrors = True )
+
+  gDB = TransferDB()
+
+  entry = TransRequestEntry(username="lintao", 
+                            submit_time = datetime.datetime.now())
+  gDB.insert_TransferRequest(entry)
