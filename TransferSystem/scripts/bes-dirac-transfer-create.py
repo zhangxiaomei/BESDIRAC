bf85053a95f8c03d0b2c77fc87d2b8dbbbf4fc02
mirguest <lintao@ihep.ac.cn>
2013-03-14 16:39:40 +0800

    Parse the command line.

--- TransferSystem/scripts/bes-dirac-transfer-create.py
+++ TransferSystem/scripts/bes-dirac-transfer-create.py
@@ -3,14 +3,27 @@
 import DIRAC
 from DIRAC.Core.Base import Script
 
+Script.setUsageMessage("""
+Create a data transfer request.
+
+Usage:
+  %s <dataset name> <src SE> <dst SE>
+""" % Script.scriptName)
+
 Script.parseCommandLine( ignoreErrors = True )
+
+args = Script.getPositionalArgs()
+if (len(args) != 3):
+  gLogger.error("Please support dataset name, src SE, dst SE.")
+  DIRAC.exit(-1)
 
 from DIRAC.Core.DISET.RPCClient import RPCClient
 
 transferRequest = RPCClient("Transfer/TransferRequest")
 
-dataset = "my-dataset"
-ep_from = "IHEP-USER"
-ep_to = "IHEPD-USER"
+dataset = args[0]
+ep_from = args[1]
+ep_to = args[2]
+
 print transferRequest.create(dataset, ep_from, ep_to)
 
