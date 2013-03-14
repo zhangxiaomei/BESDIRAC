3825c139e8a542561fad9fcf25179f4e985a5a3a
mirguest <lintao@ihep.ac.cn>
2013-03-14 16:50:49 +0800

    Add gLogger.

--- TransferSystem/Agent/helper/TransferFactory/__init__.py
+++ TransferSystem/Agent/helper/TransferFactory/__init__.py
@@ -4,10 +4,15 @@
 
 import subprocess
 
+import DIRAC
+from DIRAC import gLogger
+
 class TransferFactory(object):
   PROTOCOL = ["DIRACDMS"]
 
   def generate(self, protocol, info):
+    gLogger.info("Load Module:")
+    gLogger.info("BESDIRAC.TransferSystem.Agent.helper.TransferFactory.TransferBy%s"%(protocol))
     mod = __import__("BESDIRAC.TransferSystem.Agent.helper.TransferFactory.TransferBy%s"%(protocol),
         globals(),
         locals(),
