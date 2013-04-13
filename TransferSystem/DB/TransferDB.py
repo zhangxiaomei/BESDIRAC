0c51b003802009420b9b1708352ebefa986818f5
lintao <lintao@ihep.ac.cn>
2013-04-13 09:29:44 +0800

    add the error .

--- TransferSystem/DB/TransferDB.py
+++ TransferSystem/DB/TransferDB.py
@@ -204,11 +204,11 @@ if __name__ == "__main__":
                              start_time = None,
                              finish_time = None,
                              status = "new",
+                             error = "",
                              )
   gDB.insert_PerTransferFile(entry)
 
   filelist = map(str, range(10))
-  gDB.insert_TransferFileList(trans_id, filelist)
 
   condDict = {'trans_req_id': trans_id}
   print gDB.get_TransferFileList(condDict)
@@ -220,4 +220,7 @@ if __name__ == "__main__":
   condDict = {'name': 'my-dataset-2'}
   print gDB.get_DatasetInfo(condDict)
   condDict = {}
-  print gDB.get_Dataset(condDict)
+  res = gDB.get_Dataset(condDict)
+  if res["OK"]:
+    filelist = res["Value"]
+    gDB.insert_TransferFileList(trans_id, filelist)
