3d203650a3b30ca43d9433f88a38776b0917919f
xiaomei <zhangxm@ihep.ac.cn>
2017-05-31 10:30:24 +0200

    Update Test_dummy.py

--- Test_dummy.py
+++ Test_dummy.py
@@ -4,9 +4,7 @@
 import unittest
 import importlib
 
-
 from DIRAC import gLogger
-
 
 class AgentsTestCase( unittest.TestCase ):
   def setUp( self ):
@@ -14,8 +12,6 @@ class AgentsTestCase( unittest.TestCase ):
 
   def test1( self ):
     pass
-
-
 
 #############################################################################
 # Test Suite run
@@ -25,5 +21,3 @@ if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( AgentsTestCase )
   suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( test1) )
   testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
-
-# EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
