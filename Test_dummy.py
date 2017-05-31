1822638cafe4fed64528483bd14b588bba901f32
xiaomei <zhangxm@ihep.ac.cn>
2017-05-31 10:16:09 +0200

    Create Test_dummy.py

---
+++ Test_dummy.py
@@ -0,0 +1,31 @@
+""" Test class ""
+
+# imports
+import unittest
+import importlib
+
+
+from DIRAC import gLogger
+
+
+class AgentsTestCase( unittest.TestCase ):
+  """ Base class for the Agents test cases
+  """
+  def setUp( self ):
+    pass
+
+  def test1( self ):
+    pass
+
+
+
+#############################################################################
+# Test Suite run
+#############################################################################
+
+if __name__ == '__main__':
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( AgentsTestCase )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( test1) )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
+
+# EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
